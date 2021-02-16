d71ad4d147c798448c1f57cb52d1320efb0ad0e8
Oleg Chvirov <wowgameoleg@mail.ru>
2021-02-16 16:51:54 +0300

    Finished 2nd layout, added CalendarView

--- app/src/main/java/chvirov/com/example/work11/MainActivity.java
+++ app/src/main/java/chvirov/com/example/work11/MainActivity.java
@@ -3,10 +3,6 @@ package chvirov.com.example.work11;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 

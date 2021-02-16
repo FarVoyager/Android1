6282b90626758b10501320cc054b990facfd3d00
Oleg Chvirov <wowgameoleg@mail.ru>
2021-02-16 16:32:43 +0300

    1st layout finished, 2nd created

--- app/src/main/java/chvirov/com/example/work11/MainActivity.java
+++ app/src/main/java/chvirov/com/example/work11/MainActivity.java
@@ -17,28 +17,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button button = findViewById(R.id.button);
-        final EditText editText1 = findViewById(R.id.editText1);
-        final EditText editText2 = findViewById(R.id.editText2);
-        final TextView textView = findViewById(R.id.textView);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                try {
-                    arg1 = Integer.valueOf(editText1.getText().toString());
-                    arg2 = Integer.valueOf(editText2.getText().toString());
-                    if (arg1.equals(arg2)) {
-                        textView.setText("Равно!");
-                    } else {
-                        textView.setText("Не равно!");
-                    }
-                } catch (NumberFormatException e) {
-                    textView.setText("Нужно ввести число!");
-                }
-
-            }
-        });
+//
+//        Button button = findViewById(R.id.button);
+//        final EditText editText1 = findViewById(R.id.editText1);
+//        final EditText editText2 = findViewById(R.id.editText2);
+//        final TextView textView = findViewById(R.id.textView1);
+//
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                try {
+//                    arg1 = Integer.valueOf(editText1.getText().toString());
+//                    arg2 = Integer.valueOf(editText2.getText().toString());
+//                    if (arg1.equals(arg2)) {
+//                        textView.setText("Равно!");
+//                    } else {
+//                        textView.setText("Не равно!");
+//                    }
+//                } catch (NumberFormatException e) {
+//                    textView.setText("Нужно ввести число!");
+//                }
+//
+//            }
+//        });
     }
 }

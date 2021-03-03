14df3d65d323b9f9a9ca4177a0f6334dc0c3285c
Oleg Chvirov <wowgameoleg@mail.ru>
2021-03-03 20:25:54 +0300

    Added partial functionality of buttons (2), several bugfixes

--- app/src/main/java/chvirov/com/example/work11/MainActivity.java
+++ app/src/main/java/chvirov/com/example/work11/MainActivity.java
@@ -19,55 +19,242 @@ public class MainActivity extends AppCompatActivity {
     private TextView textViewResult;
     private TextView textViewExpression;
     private String overallText = "";
-    private final String digit1 = "1";
-    private int value1;
-    private int value2;
+    protected int valueOfFirstNumber;
     private char action = ' ';
+    protected boolean isAction = false;
+    protected int calculationResult;
+    protected boolean isActionPressed;
+
+
+    private final String digit1 = "1";
+    private final String digit2 = "2";
+    private final String digit3 = "3";
+    private final String digit4 = "4";
+    private final String digit5 = "5";
+    private final String digit6 = "6";
+    private final String digit7 = "7";
+    private final String digit8 = "8";
+    private final String digit9 = "9";
+    private final String digit0 = "0";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        isActionPressed = false;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textViewResult = findViewById(R.id.textViewResult);
         textViewExpression = findViewById(R.id.textViewExpression);
-
-        //связывание кнопок с макета с их экземплярами
-        Button button1 = findViewById(R.id.button1);
-        Button buttonPlus = findViewById(R.id.buttonPlus);
         Button buttonResult = findViewById(R.id.buttonResult);
 
 
-        button1.setOnClickListener(v -> {
-
-            //установить текст на TextView
-            textViewExpression.setText(String.format(Locale.getDefault(), overallText + digit1));
-            overallText = textViewExpression.getText().toString();
-        });
-
-        buttonPlus.setOnClickListener(v -> {
-            value1 = Integer.parseInt(overallText);
-            textViewExpression.setText(String.format(Locale.getDefault(), overallText + " + "));
-            action = '+';
-            overallText = textViewExpression.getText().toString();
-            //установить текст на TextView
-        });
-
         buttonResult.setOnClickListener(v -> {
+
+            isAction = false;
+
             if (action == '+') {
                 String[] values = overallText.split("\\+");
-                textViewResult.setText(String.format(Locale.getDefault(), Integer.toString(Integer.parseInt(values[0]) + Integer.parseInt(values[1]))));
+                calculationResult = Integer.parseInt(values[0]) + Integer.parseInt(values[1]);
+                String calculationResultStr = Integer.toString(calculationResult);
+                textViewResult.setText(String.format(Locale.getDefault(), calculationResultStr));
+                textViewExpression.setText(textViewResult.getText());
+                overallText = textViewResult.getText().toString();
+                if (calculationResult < 0) {
+                    overallText = textViewResult.getText().subSequence(1, overallText.length() - 1).toString();
+                } else {
+                    overallText = textViewResult.getText().toString();
+                }
+
+            } else if (action == '-') {
+
+                String[] values = overallText.split("-");
+                calculationResult = Integer.parseInt(values[0]) - Integer.parseInt(values[1]);
+                String calculationResultStr = Integer.toString(calculationResult);
+                textViewResult.setText(String.format(Locale.getDefault(), calculationResultStr));
+                textViewExpression.setText(textViewResult.getText());
+                overallText = textViewResult.getText().toString();
+                if (calculationResult < 0) {
+                    overallText = textViewResult.getText().subSequence(1, overallText.length()).toString();
+                } else {
+                    overallText = textViewResult.getText().toString();
+                }
+
+            } else if (action == '*') {
+
+                String[] values = overallText.split("\\*");
+                calculationResult = Integer.parseInt(values[0]) * Integer.parseInt(values[1]);
+                String calculationResultStr = Integer.toString(calculationResult);
+                textViewResult.setText(String.format(Locale.getDefault(), calculationResultStr));
+                textViewExpression.setText(textViewResult.getText());
+                overallText = textViewResult.getText().toString();
+                if (calculationResult < 0) {
+                    overallText = textViewResult.getText().subSequence(1, overallText.length() - 1).toString();
+                } else {
+                    overallText = textViewResult.getText().toString();
+                }
+
+            } else if (action == '/') {
+
+                String[] values = overallText.split("/");
+                calculationResult = Integer.parseInt(values[0]) / Integer.parseInt(values[1]);
+                String calculationResultStr = Integer.toString(calculationResult);
+                textViewResult.setText(String.format(Locale.getDefault(), calculationResultStr));
+                textViewExpression.setText(textViewResult.getText());
+                overallText = textViewResult.getText().toString();
+                if (calculationResult < 0) {
+                    overallText = textViewResult.getText().subSequence(1, overallText.length() - 1).toString();
+                } else {
+                    overallText = textViewResult.getText().toString();
+                }
+
+            } else if (action == '°') {
+
+                String[] values = overallText.split("°");
+                calculationResult = Integer.parseInt(values[1]) * 100 / Integer.parseInt(values[0]);
+                String calculationResultStr = Integer.toString(calculationResult);
+                textViewResult.setText(String.format(Locale.getDefault(), calculationResultStr));
+                textViewExpression.setText(textViewResult.getText());
+                if (calculationResult < 0) {
+                    overallText = textViewResult.getText().subSequence(1, overallText.length() - 1).toString();
+                } else {
+                    overallText = textViewResult.getText().toString();
+                }
             }
 
-
-            //установить текст на TextView
         });
+
+
+        //связывание кнопок с макета с их экземплярами
+        Button button1 = findViewById(R.id.button1);
+        Button button2 = findViewById(R.id.button2);
+        Button button3 = findViewById(R.id.button3);
+        Button button4 = findViewById(R.id.button4);
+        Button button5 = findViewById(R.id.button5);
+        Button button6 = findViewById(R.id.button6);
+        Button button7 = findViewById(R.id.button7);
+        Button button8 = findViewById(R.id.button8);
+        Button button9 = findViewById(R.id.button9);
+        Button button0 = findViewById(R.id.button0);
+
+
+        Button buttonPlus = findViewById(R.id.buttonPlus);
+        Button buttonMinus = findViewById(R.id.buttonMinus);
+        Button buttonMultiply = findViewById(R.id.buttonMultiply);
+        Button buttonDivide = findViewById(R.id.buttonDivide);
+        Button buttonPercent = findViewById(R.id.buttonPercent);
+
+        Button buttonClear = findViewById(R.id.buttonClear);
+
+
+        button0.setOnClickListener(v -> {
+            digitAssign(digit0);
+        });
+
+        button1.setOnClickListener(v -> {
+            digitAssign(digit1);
+        });
+
+        button2.setOnClickListener(v -> {
+            digitAssign(digit2);
+        });
+
+        button3.setOnClickListener(v -> {
+            digitAssign(digit3);
+        });
+
+        button4.setOnClickListener(v -> {
+            digitAssign(digit4);
+        });
+
+        button5.setOnClickListener(v -> {
+            digitAssign(digit5);
+        });
+
+        button6.setOnClickListener(v -> {
+            digitAssign(digit6);
+        });
+
+        button7.setOnClickListener(v -> {
+            digitAssign(digit7);
+        });
+
+        button8.setOnClickListener(v -> {
+            digitAssign(digit8);
+        });
+
+        button9.setOnClickListener(v -> {
+            digitAssign(digit9);
+        });
+
+
+        buttonPlus.setOnClickListener(v -> {
+
+            buttonOperation('+', "+");
+        });
+
+        buttonMinus.setOnClickListener(v -> {
+            buttonOperation('-', "-");
+        });
+
+        buttonMultiply.setOnClickListener(v -> {
+            buttonOperation('*', "*");
+        });
+
+        buttonDivide.setOnClickListener(v -> {
+            buttonOperation('/', "/");
+        });
+
+        buttonPercent.setOnClickListener(v -> {
+            buttonOperation('°', "°");
+        });
+
+        buttonClear.setOnClickListener(v -> {
+            overallText = "";
+            textViewResult.setText("");
+            textViewExpression.setText("");
+        });
+
+
+
+    }
+
+    private void digitAssign(String digit) {
+        if (overallText.equals("0")) {
+            overallText = "";
+            textViewExpression.setText(overallText);
+        }
+        textViewExpression.setText(String.format(Locale.getDefault(), overallText + digit));
+        overallText = textViewExpression.getText().toString();
+    }
+
+    private void buttonOperation(char actionVarChar, String actionTextView) {
+
+
+//        if (calculationResult < 0) {
+//            overallText = textViewResult.getText().subSequence(1, overallText.length()).toString();
+//            int subZeroCorrection = Integer.parseInt(overallText);
+//            overallText = Integer.toString(subZeroCorrection - (subZeroCorrection * 2));
+//        }
+
+
+            valueOfFirstNumber = Integer.parseInt(overallText);
+            textViewExpression.setText(String.format(Locale.getDefault(), overallText + actionTextView));
+            action = actionVarChar;
+            overallText = textViewExpression.getText().toString();
     }
 
 
-//    // Установить текст на TextView
-//    private void setTextCounter(TextView textCounter, int counter){
-//        textCounter.setText(String.format(Locale.getDefault(), "%d", counter));
-//    }
+//        if (calculationResult < 0) {
+//            overallText = textViewResult.getText().subSequence(1, overallText.length() - 1).toString();
+//            int subZeroCorrection = Integer.parseInt(overallText);
+//            overallText = Integer.toString(subZeroCorrection - (subZeroCorrection * 2));
+//        }
+
+
+
+
+
+
 
     @Override
     protected void onStart() {
